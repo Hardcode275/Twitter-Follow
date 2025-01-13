2fed5f40b280e8c92bc534677df09d6b0bc7acb8
HR <hrcode275@gmail.com>
2025-01-13 00:52:42 -0600

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,58 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
-export function App(){
-    const formatUserName = (userName) => `@${userName}` //pasar una funcion.
-   return (
-    <section className="App"> {/* al utilizar React.fragment es mejor colocarlo y quitar las letras para que se vea mejor visible */}
-        <TwitterFollowCard formatUserName={formatUserName} userName="midudev" name="Miguel Ángel Durán" /> {/*por ejemplo podemos guardar el nombre asi nomas sin el parametro name, y este se guardaria automaticamente en uno llamado children este se pasa en thitterFollowCard y se establece como name pero en vez de eso se da como children y se pasa el parametro establecido donde estaba name, automaticamente lo leera. te da un campo libre podes darle mas nombre, osea que sea mas extensible. */}
-        <TwitterFollowCard formatUserName={formatUserName} userName="hr_275" name="HR" />
-        <TwitterFollowCard formatUserName={formatUserName} userName="leanpub" name="LeanPub" />
-        <TwitterFollowCard formatUserName={formatUserName} userName="vxnder" name="Vanderhart" />
-    </section>
-   ) 
-}
+const users = [
+    {
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán',
+      isFollowing: true
+    },
+    {
+      userName: 'pheralb',
+      name: 'Pablo H.',
+      isFollowing: false
+    },
+    {
+      userName: 'PacoHdezs',
+      name: 'Paco Hdez',
+      isFollowing: true
+    },
+    {
+      userName: 'TMChein',
+      name: 'Tomas',
+      isFollowing: false
+    }
+  ]
+  
+  export function App () {
+    return (
+      <section className='App'>
+        {
+          users.map(({ userName, name, isFollowing }) => (
+            <TwitterFollowCard
+              key={userName}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterFollowCard>
+          ))
+        }
+      </section>
+    )
+  }
+
+
+//export function App(){
+  //  const formatUserName = (userName) => `@${userName}` //pasar una funcion.
+   //return (
+    //<section className="App"> {/* al utilizar React.fragment es mejor colocarlo y quitar las letras para que se vea mejor visible */}
+     //   <TwitterFollowCard formatUserName={formatUserName} userName="midudev" name="Miguel Ángel Durán" initialIsFollowing={true}/> {/*por ejemplo podemos guardar el nombre asi nomas sin el parametro name, y este se guardaria automaticamente en uno llamado children este se pasa en thitterFollowCard y se establece como name pero en vez de eso se da como children y se pasa el parametro establecido donde estaba name, automaticamente lo leera. te da un campo libre podes darle mas nombre, osea que sea mas extensible. */}
+       // <TwitterFollowCard formatUserName={formatUserName} userName="hr_275" name="HR" />
+       // <TwitterFollowCard formatUserName={formatUserName} userName="leanpub" name="LeanPub" />
+       // <TwitterFollowCard formatUserName={formatUserName} userName="vxnder" name="Vanderhart" />
+    //</section>
+//   ) 
+//}
 
 //un componente es una factoria de elementos, el elemento es el que renderiza, react renderiza elementos.
--- src/TwitterFollowCard.jsx
+++ src/TwitterFollowCard.jsx
@@ -1,12 +1,59 @@
-export function TwitterFollowCard ({formatUserName,userName, name, isFollowing}) { //si hay un valor por defecto en los parametros solo colocas el valor en este ejemplo userName = 'unknown'
+import { useState } from "react"; //importar los hooks te permiten añadir sierta funcionalidad a los componentes de react, utilidades de react para mejorar el programa // guarda variable si estamos o no estaos siguiendo a ese usuario este usestate
+export function TwitterFollowCard ({ children, userName, initialIsFollowing }) {
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+  
+    console.log('[TwitterFollowCard] render with userName: ', userName)
+  
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+      ? 'tw-followCard-button is-following'
+      : 'tw-followCard-button'
+  
+    const handleClick = () => {
+      setIsFollowing(!isFollowing)
+    }
+  
+    return (
+      <article className='tw-followCard'>
+        <header className='tw-followCard-header'>
+          <img
+            className='tw-followCard-avatar'
+            alt='El avatar de midudev'
+            src={`https://unavatar.io/${userName}`}
+          />
+          <div className='tw-followCard-info'>
+            <strong>{children}</strong>
+            <span className='tw-followCard-infoUserName'>@{userName}</span>
+          </div>
+        </header>
+  
+        <aside>
+          <button className={buttonClassName} onClick={handleClick}>
+            <span className='tw-followCard-text'>{text}</span>
+            <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+          </button>
+        </aside>
+      </article>
+    )
+  }
+/*
+export function TwitterFollowCard ({formatUserName,userName, name, initialIsFollowing}) { //si hay un valor por defecto en los parametros solo colocas el valor en este ejemplo userName = 'unknown'
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+    
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
 
     return (
         <article className='tw-followCard'>
         <header className='tw-followCard-header'>
             <img 
             className='tw-followCard-avatar'
-            src={`https://unavatar.io/substack/${userName}`} alt="avatar" /> {/*sirve para evaluar una expresion  o reutliza el parametro establecido porque sino lo toma como un string*/}
-            <div className='tw-followCard-info'>
+            src={`https://unavatar.io/substack/${userName}`} alt="avatar" /> {/*sirve para evaluar una expresion  o reutliza el parametro establecido porque sino lo toma como un string*/
+      /*      <div className='tw-followCard-info'> 
 
                 <strong>{name}</strong>
                 <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
@@ -14,12 +61,14 @@ export function TwitterFollowCard ({formatUserName,userName, name, isFollowing})
         </header>
 
         <aside>
-            <button className='tw-followCard-button'>
-                Seguir {/*lo que envuelve un elemento son sus hijos  */}
-            </button>
+        <button className={buttonClassName} onClick={handleClick}>
+          <span className='tw-followCard-text'>{text}</span>
+          <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+        </button>
         </aside>
     </article>
     )
 }
+//la base de la reutilizacion de cualquier componente en react es las propiedades, los parametros.  
 
-//la base de la reutilizacion de cualquier componente en react es las propiedades, los parametros. 
+*/
